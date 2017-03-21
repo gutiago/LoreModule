c909e5ea0c18084198527be265c539444875a835
gustavotiago-moymer <gustavo.tiago@moymer.com>
2017-03-21 18:21:24 -0300

    .

---
+++ LoreModule/LoreModule.h
@@ -0,0 +1,19 @@
+//
+//  LoreModule.h
+//  LoreModule
+//
+//  Created by Gustavo Tiago on 21/03/17.
+//  Copyright © 2017 Moymer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoreModule.
+FOUNDATION_EXPORT double LoreModuleVersionNumber;
+
+//! Project version string for LoreModule.
+FOUNDATION_EXPORT const unsigned char LoreModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoreModule/PublicHeader.h>
+
+
